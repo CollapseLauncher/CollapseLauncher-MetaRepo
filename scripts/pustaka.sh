68c720bc882efae76d26c640202aa09c1a027622
Bagus Nur Listiyono <dzakibagus@gmail.com>
2024-12-12 14:16:48 +0700

    Update CN version

--- scripts/pustaka.sh
+++ scripts/pustaka.sh
@@ -75,7 +75,7 @@ update_region() {
 # Update all regions
 update_region "Hi3SEA" "" "" ""
 update_region "Hi3Global" "" "" ""
-update_region "Hi3CN" "7.9.1" "\"\"" "7.9.0"
+update_region "Hi3CN" "" "" ""
 update_region "Hi3TW" "" "" ""
 update_region "Hi3KR" "" "" ""
 update_region "Hi3JP" "" "" ""
